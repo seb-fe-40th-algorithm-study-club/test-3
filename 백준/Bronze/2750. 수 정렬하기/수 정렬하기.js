1b97b3c2c77a0fce62fbabc0573a91df0fc6b0d4
journey <public.jinhui.kim@gmail.com>
2022-10-03 13:43:30 +0900

    [Bronze II] Title: 수 정렬하기, Time: 132 ms, Memory: 9472 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.js
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.js
@@ -2,9 +2,4 @@ const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 let [N, ...input] = fs.readFileSync(filePath).toString().trim().split("\n");
 
-console.log(
-  input
-    .filter((v, i) => input.indexOf(v) === i)
-    .sort((a, b) => a - b)
-    .join("\n")
-);
+console.log(input.sort((a, b) => a - b).join("\n"));
